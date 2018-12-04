096b3091ef5acb47f10976a5db3f6c0d50196f21
yousefmasry4 <35680381+yousefmasry4@users.noreply.github.com>
2018-12-04 22:21:07 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int cp(char s1[50],char s2[],int n){
-    for(int i=0;i<n;i++){
-        if(s1[i] ==0){
-            break;
-        }
-        else{
-            s2[i]=s1[i];
-        }
-    }
-}
-
-int main()
+struct birthday
 {
-    char s1[50],s2[50];
-    gets(s1);
-    int n;
-    scanf("%d",&n);
-    cp(s1,s2,n);
-    puts(s2);
-    return 0;
+ int day;
+ int month;
+int year;
+
+
+}Date;
+ int main (){
+  char *ps;
+  printf("Enter your Birthday \n");
+  scanf("%d/%d/%d",& Date.day,&Date.month,&Date.year);
+  printf(" your Birthday BEFORE :%d / %d  /%d ",Date.day,Date.month,Date.year);
+
+
+  switch (Date.month)
+ {  case 1:
+    ps="January";
+     break;
+  case 2:
+    ps="Febrary";
+     break;
+ case 5:
+    ps="Mai";
+     break;
+ case 6:
+    ps="June";
+     break;
+case 7:
+    ps="July";
+     break;
+case 8:
+    ps="Aug";
+     break;
+case 9:
+    ps="sept";
+     break;
+case 10:
+    ps="october";
+     break;
+case 11:
+    ps="November";
+     break;
+case 12:
+    ps="December";
+     break;
+ default :
+  printf("error");
+ break;
+
 }
+
+printf("your Bithday After: %s / %d /%d",ps,Date.day,Date.year);
+}
+
